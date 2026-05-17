f7a442a0befb0cee7089c0a7ee2515fc7c8fb57e
Etienne Harode Louembet <Etiennelouembet41@gmail.com>
2026-05-17 18:48:50 +0200

    Create custom user model and core business models

--- backend/addresses/models.py
+++ backend/addresses/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.conf import settings
+from core.models import MalaisianCity
 # Create your models here.
+
+class Address(models.Model):
+    user=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="addresses"
+        
+    )
+    
+    city=models.ForeignKey(
+        MalaisianCity,
+        on_delete=models.CASCADE,
+    )
+    
+    address_line=models.TextField()
+
+    postal_code=models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    
+    is_default=models.BooleanField(default=False)
+
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email}-{self.city.name}"
+
+
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+class OriginCountry(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+    
+    code=models.CharField(max_length=10, unique=True)
+
+    is_activate=models.BooleanField(default=True)
+
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    
+
+class MalaisianCity(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+
+    delivery_price=models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+    )
+    
+    estimation_delivery_days=models.IntegerField(default=1)
+
+    is_activate=models.BooleanField(default=True)
+
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
