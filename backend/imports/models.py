7a58e2014b71e11f6b438d77aaf9c1fa8ab6425b
Etienne Harode Louembet <Etiennelouembet41@gmail.com>
2026-05-17 20:50:45 +0200

    Create import and order system models

--- backend/imports/models.py
+++ backend/imports/models.py
@@ -1,3 +1,55 @@
 from django.db import models
-
+from cars.models import Car
 # Create your models here.
+
+class ImportInfo(models.Model):
+    
+    IMPORT_STATUS_CHOICES=(
+        ("pending","Pending"),
+        ("confirmed", "Confirmed"),
+        ("supplier_purchase", "Supplier Purchase"),
+        ("documents_preparation", "documents in preparation"),
+        ("international_shipping", "International Shipping"),
+        ("malaysia_customs","Malaysian Customs"),
+        ("local_delivery", "Local Delivery"),
+        ("delivered","Delivered"),
+        
+    )
+    
+    car=models.ForeignKey(
+        Car,
+        on_delete=models.CASCADE,
+        related_name="import_info",
+    )
+    
+    estimated_import_cost=models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    
+    estimated_import_days=models.PositiveIntegerField()
+
+    required_documents=models.TextField()
+
+    customs_fees =models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+    )
+    
+    status=models.CharField(
+        max_length=50,
+        choices=IMPORT_STATUS_CHOICES,
+        default="pending",
+    )
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Import Info - {self.car}"
+    
+    
+
+    
+    
--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,104 @@
 from django.db import models
-
+from django.conf import settings
+from cars.models import Car
+from core.models import MalaisianCity
+from addresses.models import Address
 # Create your models here.
+
+class Order(models.Model):
+    OREDER_STATUS_CHOICES=(
+        ("pending","Pending"),
+        ("confirmed","Confirmed"),
+        ("processing","Processing"),
+        ("importing","import in progress"),
+        ("delivering","Delivering in progress"),
+        ("completed","Completed"),
+        ("cancelled","Cancelled"),
+    )
+    
+    PAYMENT_STATUS_CHOICES=(
+        ("unpaid","Non payé"),
+        ("deposit_paid","Acompte payé"),
+        ("paid","Payé"),
+        ("failed","Echec Paiement"),
+        ("refunded","Remboursé"),
+    )
+    
+    user=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
+    )
+    
+    car=models.ForeignKey(
+        Car,
+        on_delete=models.PROTECT,
+        related_name="orders",
+    )
+    
+    delivery_city=models.ForeignKey(
+        Address,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+    
+    car_price=models.DecimalField(max_digits=12,decimal_places=2)
+
+    import_fees=models.DecimalField(max_digits=12,decimal_places=2)
+
+    delivery_fees=models.DecimalField(max_digits=12,decimal_places=2)
+
+    total_price=models.DecimalField(max_digits=12, decimal_places=2)
+
+    status=models.CharField(
+        max_length=50,
+        choices=OREDER_STATUS_CHOICES,
+        default="pending",
+        
+    )
+    
+    payment_status=models.CharField(
+        max_length=50,
+        choices=PAYMENT_STATUS_CHOICES,
+        default="unpaid",
+    )
+    
+    stripe_payment_id=models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+    )
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    updated_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=["-created_at"]
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.user.email}"
+
+
+class OrderItem(models.Model):
+    
+    orders=models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="items",
+    )
+    
+    car=models.ForeignKey(
+        Car,
+        on_delete=models.PROTECT
+    )
+    
+    quantity=models.PositiveIntegerField(default=1)
+
+    price=models.DecimalField(max_digits=12,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.car} x {self.quantity}"
+    
+    
+    
